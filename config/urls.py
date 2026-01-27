c8fe8c0f72e1bc9784e5cebd6ab540cd90e0afa9
djumanov <djumanovdev@gmail.com>
2026-01-27 09:52:38 +0500

    home page template

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("users.urls")),
+    path("", lambda request: render(request, "home.html")),
 ]
